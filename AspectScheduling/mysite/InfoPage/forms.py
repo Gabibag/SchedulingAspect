b3e1be8ba7e5eee6d6c01855cebee6decb4d842d
Gabibag <97263038+Gabibag@users.noreply.github.com>
2022-12-13 21:09:47 +0000

    just adding some small things

--- AspectScheduling/mysite/InfoPage/forms.py
+++ AspectScheduling/mysite/InfoPage/forms.py
@@ -2,4 +2,5 @@ from django import forms
 
 class LoginForm(forms.Form):
     login = forms.CharField()
-    password = forms.CharField()
+    password = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'passwd'}))
