3c375d1b20ccd3e6a79aed589b74d966bc68b9ee
Gabibag <gabihadlin@gmail.com>
2022-12-14 12:03:21 -0800

    Login theming is complete! Now with spy security

--- AspectScheduling/mysite/InfoPage/forms.py
+++ AspectScheduling/mysite/InfoPage/forms.py
@@ -1,7 +1,15 @@
 from django import forms
 from django.utils.safestring import mark_safe
-class LoginForm(forms.Form):
-    login = forms.CharField(label='ID Number')
+
+
     
-    password = forms.CharField(label=mark_safe('<br /> Password'),
-        widget=forms.PasswordInput(attrs={'class': 'passwd'}))
+        
+class LoginForm(forms.Form):
+  def __init__(self, *args, **kwargs):
+      super().__init__(*args, **kwargs)
+      self.label_suffix = " "  # Removes : as label suffix
+  login = forms.CharField(label=' ', widget=forms.TextInput(
+      attrs={'placeholder': 'ID Number', 'class': 'passwd', 'style': 'height: 25px;'}))
+  
+  password = forms.CharField(label=mark_safe('<br /> '),
+                              widget=forms.PasswordInput(attrs={'placeholder': 'Password', 'class': 'passwd', 'style': 'height: 25px;'}))
