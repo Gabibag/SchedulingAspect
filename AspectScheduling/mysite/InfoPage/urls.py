5ff9858ca8b6df37c571373de83a6f6632b44337
Gabibag <97263038+Gabibag@users.noreply.github.com>
2023-01-20 17:10:35 -0800

    kind of made it a bit more grid like, still not quiet done, but ig this is pretty okay.

--- AspectScheduling/mysite/InfoPage/urls.py
+++ AspectScheduling/mysite/InfoPage/urls.py
@@ -1,11 +1,12 @@
-
 from django.contrib import admin
 from django.urls import include, path
 from django.shortcuts import redirect
 from . import views
 
+
 def Redirect(request):
     return redirect('/main')
+
 
 urlpatterns = [
     path('logout', views.Logout),
